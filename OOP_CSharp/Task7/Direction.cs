eae695e3174af27d3dc37ebd796a89eef14868be
aleygv <aleygv@bk.ru>
2025-09-04 18:26:17 +0300

    Complete 7 task oop

---
+++ OOP_CSharp/Task7/Direction.cs
@@ -0,0 +1,48 @@
+﻿namespace Task7;
+
+public class Direction
+{
+    public string Title { get; }
+    public int Passengers { get; private set; }
+    private Train _train;
+
+    public Direction(string title)
+    {
+        Title = title;
+    }
+
+    public void SellTickets()
+    {
+        Random random = new Random();
+        Passengers = random.Next(200, 1500);
+        Console.WriteLine($"Продано {Passengers} билетов на направление {Title}");
+    }
+    public void FormTrain()
+    {
+        _train = new Train();
+        Random random = new Random();
+        while (_train.FreeSeats < Passengers)
+        {
+            int capacity = random.Next(40, 100);
+            _train.AddWagon(new Wagon(capacity));
+        }
+
+        int notSeatedPassengers = Passengers;
+        foreach (Wagon trainWagon in _train.Wagons)
+        {
+            while (notSeatedPassengers >0 && trainWagon.FreeSeats > 0)
+            {
+                trainWagon.OccupiedSeats++;
+                notSeatedPassengers--;
+            }
+        }
+        
+        Console.WriteLine($"Поезд сформирован: {_train.Wagons.Count} вагонов.");
+    }
+
+    public void SendTrain()
+    {
+        Console.WriteLine($"Поезд по направлению {Title} отправляется с {Passengers} пассажирами");
+    }
+
+}
---
+++ OOP_CSharp/Task7/Program.cs
@@ -0,0 +1,44 @@
+﻿using Task7;
+
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+        Direction direction = null;
+        
+        while (true)
+        {
+            Console.WriteLine($"Выберете действие:\n" +
+                              $"[1] Создать направление\n" +
+                              $"[2] Продать билеты\n" +
+                              $"[3] Сформировать поезд\n" +
+                              $"[4] Отправить поезд");
+            
+            string input1 = Console.ReadLine();
+            
+            switch (input1)
+            {
+                case "1":
+                    Console.Write($"Введите название направления: ");
+                    string input2 = Console.ReadLine();
+                    direction = new Direction(input2);
+                    break;
+
+                case "2":
+                    direction.SellTickets();
+                    break;
+
+                case "3":
+                    direction.FormTrain();
+                    break;
+
+                case "4":
+                    direction.SendTrain();
+                    break;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
