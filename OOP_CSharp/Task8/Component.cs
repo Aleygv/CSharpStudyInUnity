9868d9f87bc46b127d5b89f7c945c7fb68a63285
aleygv <aleygv@bk.ru>
2025-09-06 21:01:07 +0300

    Complete task 8 oop

---
+++ OOP_CSharp/Task8/Component.cs
@@ -0,0 +1,18 @@
+﻿namespace Task8;
+
+public class Component
+{
+    public ComponentType Type { get; }
+
+    public Component(ComponentType type)
+    {
+        Type = type;
+    }
+}
+
+public enum ComponentType
+{
+    Brakes,
+    FuelFilter,
+    SparkPlugs
+}
---
+++ OOP_CSharp/Task8/Program.cs
@@ -0,0 +1,40 @@
+﻿using Task8;
+
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+        bool _isWorking = true;
+        
+        CarService carService = new CarService(1000);
+
+        while (_isWorking)
+        {
+            carService.EvaluateCar();
+            Console.WriteLine();
+            
+            Console.WriteLine($"Выберете действие:\n" +
+                              $"[1] Посмотреть склад\n" +
+                              $"[2] Починить автомобиль\n" +
+                              $"[3] Выход");
+            string choice1 = Console.ReadLine();
+            if (choice1 == "1")
+            {
+                carService.Warehouse.ShowComponents();
+            }
+
+            if (choice1 == "2")
+            {
+                carService.FixCar();
+            }
+
+            if (choice1 == "3")
+            {
+                _isWorking = false;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
